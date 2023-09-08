589175c241d38fd86a3938c17b18a28619b7aca5
Valentina De Mite <valesakura97@gmail.com>
2023-09-08 19:32:35 +0200

    Fixed folder

---
+++ main.js
@@ -0,0 +1,151 @@
+/*Ricreiamo un feed social aggiungendo al layout dello starter kit di base fornito, il nostro script JS in cui:
+
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+- id del post, numero progressivo da 1 a n
+- nome autore,
+- foto autore,
+- data in formato americano (mm-gg-yyyy),
+- testo del post,
+- immagine (non tutti i post devono avere una immagine),
+- numero di likes.
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+
+Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+Milestone 3
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+BONUS
+Formattare le date in formato italiano (gg/mm/aaaa)
+Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+*/
+const posts = [
+  {
+    id: 1,
+    content:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    media: "https://unsplash.it/600/300?image=171",
+    author: {
+      name: "Phil Mangione",
+      image: "https://unsplash.it/300/300?image=15",
+    },
+    likes: 80,
+    created: "2021-06-25",
+    clicked: false,
+  },
+  {
+    id: 2,
+    content:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    media: "https://unsplash.it/600/400?image=112",
+    author: {
+      name: "Sofia Perlari",
+      image: "https://unsplash.it/300/300?image=10",
+    },
+    likes: 120,
+    created: "2021-09-03",
+    clicked: false,
+  },
+  {
+    id: 3,
+    content:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    media: "https://unsplash.it/600/400?image=234",
+    author: {
+      name: "Chiara Passaro",
+      image: "https://unsplash.it/300/300?image=20",
+    },
+    likes: 78,
+    created: "2021-05-15",
+    clicked: false,
+  },
+  {
+    id: 4,
+    content:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    media: "https://unsplash.it/600/400?image=24",
+    author: {
+      name: "Luca Formicola",
+      image: "https://unsplash.it/300/300?image=90",
+    },
+    likes: 56,
+    created: "2021-04-03",
+    clicked: false,
+  },
+  {
+    id: 5,
+    content:
+      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    media: "https://unsplash.it/600/400?image=534",
+    author: {
+      name: "Alessandro Sainato",
+      image: "https://unsplash.it/300/300?image=29",
+    },
+    likes: 95,
+    created: "2021-03-05",
+    clicked: false,
+  },
+];
+
+//effettuo il console log delgi oggetti dell'array e creo il markup
+
+posts.forEach((element) => {
+  //   console.log(element.likes);
+
+  let postMarkup = `<div class="post my-4 rounded">
+<div class="post__header">
+    <div class="post-meta">
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${element.author.image}" >
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${element.author.name}</div>
+            <div class="post-meta__time">${element.created}</div>
+        </div>
+    </div>
+</div>
+<div class="post__text">${element.content}</div>
+<div class="post__image">
+    <img src="${element.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" id ="likeButton${element.id}" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter${element.id}" class="js-likes-counter">${element.likes}</b> persone
+        </div>
+    </div>
+</div>
+</div>`;
+  console.log(postMarkup);
+  document
+    .getElementById("postMarkup")
+    .insertAdjacentHTML("beforeend", postMarkup);
+  console.log(document.getElementById("postMarkup"));
+  /* faccio diventare ogni post unico*/
+  document
+    .getElementById("likeButton" + element.id)
+    .addEventListener("click", function () {
+      if (element.clicked == false) {
+        element.likes += 1;
+        element.clicked = true;
+        console.log(element.likes);
+      } else {
+        element.likes -= 1;
+        element.clicked = false;
+        console.log(element.likes);
+      }
+      document.getElementById("like-counter" + element.id).innerText =
+        element.likes;
+    });
+});
